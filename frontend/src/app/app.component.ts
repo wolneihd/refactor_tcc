a493d08ed21699a3ae38b7491316c5f09375886c
Wolnei Hellmann Dircksen <wolneihd@gmail.com>
2025-01-05 00:01:07 -0300

    update frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListaDadosComponent } from './lista-dados/lista-dados.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListaDadosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
 }
