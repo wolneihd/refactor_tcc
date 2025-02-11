59f2d689ba0c0335ea233e392c54ef99e4a2669c
Wolnei Hellmann Dircksen <wolneihd@gmail.com>
2025-02-11 11:03:28 -0300

    wip

--- api/api.py
+++ api/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from repository import select_all_mensagens
 
@@ -7,18 +7,26 @@ def montar_API():
     app = Flask(__name__)
     CORS(app)
 
-    # consultar todas capitais:
+    # obter todas as mensagens:
     @app.route('/', methods=['GET'])
     def todas_mensagens():
         data = select_all_mensagens()  
         return jsonify(data)  
     
-    # consultar todas capitais:
+    # responder mensagem do usuário:
     @app.route('/responder', methods=['POST'])
     def responder_mensagens():
         return jsonify({'teste': [1,2]})  
+    
+    # salvar mensagem do totem de feedback:
+    @app.route('/totem', methods=['POST'])
+    def salvar_mensagem():
+        # Obter dados do corpo da requisição
+        dados = request.get_json()
+        print(dados)
+        return jsonify({'teste': [1,2]})  
 
-    app.run(port=5001,host='localhost',debug=True)
+    app.run(port=5000,host="0.0.0.0",debug=True)
 
 if __name__ == "__main__":
     montar_API()
